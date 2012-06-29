d2ff124ed6ea8ed9b59e92aa822a84ef74229444
Juho Rutila <juho.rutila@iki.fi>
2012-06-29 09:27:46 +0300

    Added initial setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name = "django-stables",
+    version = "0.0",
+    packages = [],
+    author = "Juho Rutila",
+    author_email = "juho.rutila@iki.fi",
+    description = "Horse stable management package",
+    url = "http://rutila.fi",
+    include_package_data = True
+)
