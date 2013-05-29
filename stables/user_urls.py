bbee3f85ed98da93f19ec1240053999a3637941f
Juho Rutila <juho.rutila@iki.fi>
2013-05-29 16:04:15 +0300

    Username with a period does not crash dashboard.
    Fixes #8.

--- stables/user_urls.py
+++ stables/user_urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^user/$', 'stables.views.view_user'),
-    url(r'^user/(?P<username>\w+)/$', 'stables.views.view_user'),
+    url(r'^user/(?P<username>[\w\.]+)/$', 'stables.views.view_user'),
     # For staff
     url(r'^riderlevels', 'stables.views.update_rider_levels'),
-    url(r'^user/(?P<username>\w+)/addtickets/$', 'stables.views.add_tickets'),
+    url(r'^user/(?P<username>[\w\.]+)/addtickets/$', 'stables.views.add_tickets'),
 )
