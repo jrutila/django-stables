018dec084a088fd7d80980f1965ba950655aaad5
Juho Rutila <juho.rutila@iki.fi>
2013-07-25 22:21:16 +0300

    Course detailview.

--- stables/urls/course.py
+++ stables/urls/course.py
@@ -3,10 +3,11 @@ from django.conf.urls import *
 from stables.views import CourseCreate
 from stables.views import CourseUpdate
 from stables.views import CourseAddEvent
+from stables.views import ViewCourse
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/change/', CourseUpdate.as_view(), name="change_course"),
     url(r'^(?P<pk>\d+)/addevent/', CourseAddEvent.as_view(), name="add_event"),
-    url(r'^(?P<course_id>\d+)/', 'stables.views.view_course'),
+    url(r'^(?P<pk>\d+)/', ViewCourse.as_view(), name='view_course'),
     url(r'^add/', CourseCreate.as_view(), name="add_course"),
 )
--- stables/views/course.py
+++ stables/views/course.py
@@ -1,9 +1,20 @@
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic import FormView
+from django.views.generic import DetailView
 from stables.models import Course
 from stables.forms import CourseForm
 from stables.forms import AddEventForm
+from datetime import *
+
+class ViewCourse(DetailView):
+    model = Course
+    template_name = 'stables/course/course.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ViewCourse, self).get_context_data(**kwargs)
+        context['occurrences'] = context['object'].get_occurrences(start=date.today()-timedelta(days=7))
+        return context
 
 class CourseCreate(CreateView):
     model = Course
