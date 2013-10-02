5ae7689bb9890e957f3bdb7f18d73117f3e2a82b
Juho Rutila <juho.rutila@iki.fi>
2013-10-02 15:57:14 +0300

    Fix setup for package data.

--- setup.py
+++ setup.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import os
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+myapp_dir = 'stables'
+
+for dirpath, dirnames, filenames in os.walk(myapp_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        for f in filenames:
+            data_files.append(os.path.join(dirpath.split('/', 1)[1], f))
+
 setup(
     name = "django-stables",
     version = "dev",
     packages=find_packages(),
-    include_package_data=True,
+    package_data={ 'stables': data_files},
     author = "Juho Rutila",
     author_email = "juho.rutila@iki.fi",
     description = "Horse stable management package",
@@ -24,5 +56,4 @@ setup(
       'http://github.com/jrutila/django-schedule/tarball/master#egg=django-schedule',
       'http://github.com/jrutila/django-reporting/tarball/master#egg=django-reporting-dev',
     ]
-
 )
