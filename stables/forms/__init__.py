5986006742db400405012fef6a49f714cb6207ec
Juho Rutila <juho.rutila@iki.fi>
2013-12-27 18:36:21 +0200

    Remove participation form reference.

--- stables/forms/__init__.py
+++ stables/forms/__init__.py
@@ -1,6 +1,5 @@
 from stables.forms.course import *
 from stables.forms.user import *
-from stables.forms.participation import *
 from stables.forms.horse import *
 from stables.forms.financial import *
 from stables.forms.accident import *
