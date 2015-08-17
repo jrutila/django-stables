f8deed02696a2b099bced71218e37dab536bf153
Juho Rutila <juho.rutila@iki.fi>
2015-08-18 01:30:51 +0300

    Django scheduler upgrade

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=[
       "Babel",
       "isoweek>=1.2.0",
-      'django-scheduler==0.7.1-2',
+      'django-scheduler==0.7.1-3',
       'django-reversion==1.7.1',
       'django-sekizai',
       'django-crispy-forms',
@@ -61,7 +61,7 @@ setup(
     ],
     zip_safe=False,
     dependency_links = [
-      'http://github.com/jrutila/django-schedule/tarball/master#egg=django-scheduler-0.7.1-2',
+      'http://github.com/jrutila/django-schedule/tarball/master#egg=django-scheduler-0.7.1-3',
       'http://github.com/jrutila/django-reportengine/tarball/master#egg=django-reportengine-0.3.1.1',
       'http://github.com/jrutila/backbone-tastypie/tarball/master#egg=backbone-tastypie-dev',
     ]
