74cf7eca0d8e21f8c973fec730783635828a3d75
Juho Rutila <juho.rutila@iki.fi>
2015-10-09 08:54:16 +0300

    Working copy_db

--- production/common/management/commands/copy_db.py
+++ production/common/management/commands/copy_db.py
@@ -1,3 +1,5 @@
+from io import StringIO
+from django.contrib.contenttypes.models import ContentType
 from django.core.management import BaseCommand
 from django.db import connections
 from customers.models import Client
@@ -21,16 +23,8 @@ def dictfetchall(cursor):
         for row in cursor.fetchall()
     ]
 
-def insert(data, name, opts):
-    conv = opts["convert"] if "convert" in opts else None
-    colnames = [x for x in data.keys() if "missing" not in opts or x not in opts["missing"]]
-    cols = ['"%s"' % x for x in colnames]
-    vals = [conv[x](data[x]) if conv and x in conv else data[x] for x in colnames]
-    vals_str_list = ["%s"] * len(vals)
-    vals_str = ", ".join(vals_str_list)
 
-    return ("INSERT INTO {name} ({cols}) VALUES ({vals_str})".format(
-                   cols = ", ".join(cols), vals_str = vals_str, name = name), vals)
+
 
 class Command(BaseCommand):
     help = 'Copy old stables database to the new one'
@@ -40,7 +34,11 @@ class Command(BaseCommand):
         parser.add_argument('--skip',
             dest='skip',
             default=False,
-            help='Skip tenant creation (They should exists already)')
+            help='Skip the db tables till')
+        parser.add_argument('--tenant',
+            dest='tenant',
+            default=False,
+            help='Run only this tenant')
         #parser.add_argument('poll_id', nargs='+', type=int)
 
     def handle(self, *args, **options):
@@ -48,14 +46,6 @@ class Command(BaseCommand):
         w = connections['default'].cursor()
         c.execute("SELECT * FROM tenant_client WHERE schema_name != 'public'")
         result = dictfetchall(c)
-
-        if not options["skip"]:
-            for tenant in result:
-                if (tenant["schema_name"] != "demo"): break
-                del tenant["id"]
-                t = Client(**tenant)
-                t.save()
-                self.stdout.write("Created client "+tenant['domain_url'])
 
         tables = OrderedDict()
         tables['stables_accidenttype'] = {}
@@ -65,7 +55,7 @@ class Command(BaseCommand):
         tables['schedule_rule'] = {}
         tables['schedule_event'] = { "missing": ["priority"]}
         tables['schedule_occurrence'] = {}
-        tables['stables_shop_product'] = { }
+        tables['stables_shop_product'] = { "convert": { "polymorphic_ctype_id": "content_type" }}
         tables['shop_cart'] = {}
         tables['shop_cartitem'] = {}
         tables['shop_order'] = {}
@@ -79,8 +69,8 @@ class Command(BaseCommand):
         tables['stables_customerinfo'] = {}
         tables['stables_riderinfo'] = {}
         tables['stables_accident'] = {}
-        tables['stables_course_allowed_levels'] = {}
         tables['stables_riderlevel'] = {}
+        tables['stables_course_allowed_levels'] = {}
         tables['stables_course_events'] = {}
         tables['stables_course_ticket_type'] = {}
         tables['stables_userprofile'] = {}
@@ -100,12 +90,28 @@ class Command(BaseCommand):
         tables['stables_shop_enrollproductactivator'] = {}
         tables['stables_shop_partshorturl'] = {}
         tables['stables_shop_productactivator'] = {}
-        tables['stables_shop_ticketproduct'] = { "convert": { "duration": lambda x: "%s second" % int(x/1000000) }}
-        tables['stables_shop_ticketproductactivator'] = {}
+        tables['stables_shop_ticketproduct'] = { "convert": { "duration": lambda x: "%s second" % int(x/1000000) if x else "\\N" }}
+        tables['stables_shop_ticketproductactivator'] = { "convert": { "duration": lambda x: "%s second" % int(x/1000000) if x else "\\N" }}
         tables['stables_transaction'] = {}
         tables['stables_ticket'] = {}
 
         for tenant in result:
+
+            if "tenant" in options:
+                if (options["tenant"] and tenant["schema_name"] != options["tenant"]):
+                    continue
+
+            connections["default"].set_schema("public")
+            if not options["skip"]:
+                #if (tenant["schema_name"] != "demo"): break
+                del tenant["id"]
+                t = Client(**tenant)
+                t.save()
+                self.stdout.write("Created client "+tenant['domain_url'])
+
+            connections["old"].set_schema(tenant["schema_name"])
+            connections["default"].set_schema(tenant["schema_name"])
+
             skipping = False
             if isinstance(options["skip"], str):
                 skipping = options["skip"]
@@ -116,9 +122,44 @@ class Command(BaseCommand):
                     self.stdout.write("Skipped "+tname)
                     continue
                 toname = t["table"] if "table" in t else tname
-                self.stdout.write("Writing %s to %s" % (tname, toname))
                 c.execute("SELECT * FROM %s" % tname)
                 data = dictfetchall(c)
-                for d in data:
-                    (sql, vals) = insert(d, toname, t)
-                    w.execute(sql, vals)
+                opts = t
+                fd = ""
+                conv = opts["convert"] if "convert" in opts else None
+                counter = 0
+                if (len(data)):
+                    self.stdout.write("Writing %s to %s" % (tname, toname))
+                    colnames = [x for x in data[0].keys() if "missing" not in opts or x not in opts["missing"]]
+                    for d in data:
+                        counter = counter + 1
+                        vals = []
+                        for x in colnames:
+                            dd = d[x]
+                            if conv and x in conv:
+                                if (conv[x] == "content_type"):
+                                    oldct = ContentType.objects.using("old").get(id=dd)
+                                    newct = ContentType.objects.using("default").get(model=oldct.model)
+                                    vals.append(newct.id)
+                                else:
+                                    vals.append(conv[x](dd))
+                            else:
+                                vals.append(default_conv(dd))
+                        #vals = [conv[x](d[x]) if conv and x in conv else default_conv(d[x]) for x in colnames]
+                        fd = str(fd) + "\t".join([str(x) for x in vals]) + "\n"
+                        #(sql, vals) = insert(d, toname, t)
+                        #w.execute(sql, vals)
+                    self.stdout.write(",".join(colnames) + " -> "+toname)
+                    self.stdout.write(fd)
+                    w.copy_from(StringIO(fd), toname, columns=['"'+x+'"' for x in colnames])
+                else:
+                    self.stdout.write("No data for %s" % toname)
+
+def default_conv(value):
+    if str(value) == "True":
+        return "t"
+    if str(value) == "False":
+        return "f"
+    if value == None:
+        return "\\N"
+    return str(value).strip().replace("\r", "").replace("\n", "\\n")
