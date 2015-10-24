fe7f445caf8956eb24178bb44844769eb4bb9ee4
Juho Rutila <juho.rutila@iki.fi>
2015-10-25 00:36:28 +0300

    Tenant aware tastypie cache

--- stables/backbone/__init__.py
+++ stables/backbone/__init__.py
@@ -2,6 +2,7 @@ import logging
 
 from tastypie.cache import SimpleCache
 from tastypie.authentication import SessionAuthentication
+from django.db import connection
 
 logger = logging.getLogger(__name__)
 
@@ -15,8 +16,15 @@ class ApiList(list):
 
 class ShortClientCache(SimpleCache):
     def cache_control(self):
-
         control = super(ShortClientCache, self).cache_control()
         control['max-age'] = 10
         control['s-maxage'] = 10
         return control
+
+    def get(self, key, **kwargs):
+        key = connection.schema_name +  ":" + key
+        return super().get(key, **kwargs)
+
+    def set(self, key, value, timeout=None):
+        key = connection.schema_name +  ":" + key
+        super().set(key, value, timeout)
